9143670c00cac16b4e5d60ca1a5b640a1fe38453
Knovengel-Nova <knovengel@gmail.com>
2025-02-20 10:12:34 +0530

    Player class edited. GameBoard class constructor created.

--- GameBoard.java
+++ GameBoard.java
@@ -2,4 +2,10 @@ package Tic_Tac_Toe;
 
 public class GameBoard {
     private char[] pos = new char[9];
+
+    GameBoard(){
+        for(int i=0; i<9; i++){
+            pos[i] = ' ';
+        }
+    }
 }
--- Player.java
+++ Player.java
@@ -1,6 +1,27 @@
 package Tic_Tac_Toe;
 
 public class Player {
+    private char playerMove;
+    private int playerScore;
 
-    
+
+    public char getPlayerMove(){
+        return this.playerMove;
+    }
+
+    public void setPlayerMove(char move){
+        this.playerMove = move;
+    }
+
+    public int getPlayerScore(){
+        return this.playerScore;
+    }
+
+    public void setPlayerScore(int score){
+        this.playerScore = score;
+    }
+
+    Player(char c){
+        this.playerMove = c;
+    }
 }
